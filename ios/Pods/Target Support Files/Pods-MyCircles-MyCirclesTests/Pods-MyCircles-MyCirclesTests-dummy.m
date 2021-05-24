ca58e73ec86f3a37e2f419abc07ea6c96a6d95d6
Atit Gupta <atit_gupta@intuit.com>
2021-05-23 22:19:31 -0700

    app setup

---
+++ ios/Pods/Target Support Files/Pods-MyCircles-MyCirclesTests/Pods-MyCircles-MyCirclesTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_MyCircles_MyCirclesTests : NSObject
+@end
+@implementation PodsDummy_Pods_MyCircles_MyCirclesTests
+@end
---
+++ ios/Pods/Target Support Files/Pods-MyCircles/Pods-MyCircles-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_MyCircles : NSObject
+@end
+@implementation PodsDummy_Pods_MyCircles
+@end
